c966476af1859ebac960bd259de1e4e80c0257eb
eslupmi101 <ajsen-andreev@yandex.ru>
2024-07-24 04:35:11 +0900

    Fix .gitignore for tawilind django And fix admin telegram profile

--- src/postik/users/admin.py
+++ src/postik/users/admin.py
@@ -7,7 +7,6 @@ class TelegramProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'telegram_id', 'username', 'image', 'bio')
     search_fields = ('user__username', 'telegram_id', 'username')
     list_filter = ('user',)
-    readonly_fields = ('telegram_id',)
 
 
 class BotAccessTokenAdmin(admin.ModelAdmin):
