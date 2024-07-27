74cc1efb6f4ade1db0374c47e179091fd346bc05
eslupmi101 <ajsen-andreev@yandex.ru>
2024-07-27 23:44:48 +0900

    Fix preload url Add responsive buy button oncard

--- src/postik/postik/urls.py
+++ src/postik/postik/urls.py
@@ -20,4 +20,4 @@ handler403 = 'core.views.permission_denied'
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    path("__reload__/", include("django_browser_reload.urls")),
+    urlpatterns += path("__reload__/", include("django_browser_reload.urls")),
